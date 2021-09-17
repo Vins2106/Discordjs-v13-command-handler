8f3e0f502399804062b0182f317cccabb4927999
Glitch (urlcord) <none>
2021-09-17 10:03:33 +0000

    🎊⭐️ Checkpoint ./src/website/app.js:26244623/566 ./package.json:26244623/869 ./src/website/public/style.css:26244623/231 ./src/website/views/parts/head.ejs:26244623/2173

--- src/website/app.js
+++ src/website/app.js
@@ -1,3 +1,17 @@
 module.exports = async client => {
   
+  const express = require("express");
+  const app = express();
+  
+  app.set("views", "/app/src/website/views");
+  app.use(express.static(__dirname + "/public"));  
+  
+  app.get("/", async (req, res) => {
+    res.render("index.ejs")
+  });
+  
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("[WEBSITE] running on port 3000")
+  })
+  
 };
